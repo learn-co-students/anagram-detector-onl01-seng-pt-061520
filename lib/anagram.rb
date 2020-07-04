051a92ad609893e891a1cb1cb5eb6967a5c66f21
IgoVeyner <igorveyner95@gmail.com>
2020-07-04 18:32:42 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,11 +7,13 @@ class Anagram
     end
 
     def match(possible_anagram_array)
-        result = []
-        possible_anagram_array.each do |word|     
-            result << word if word.split("").sort == @word.split("").sort
-        end
-        result
+        # result = []
+        # possible_anagram_array.each do |word|     
+        #     result << word if word.split("").sort == @word.split("").sort
+        # end
+        # result
+
+        possible_anagram_array.select {|word| word.split("").sort == @word.split("").sort}
     end
 
 end
