57394bdc404c5b7102c794f31304d440a4a9fe38
kowalix007 L.K <kowalix007@gmail.com>
2020-07-08 23:55:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+ attr_accessor :word
+
+ def initialize(word)
+   @word = word
+ end
+
+ def match(anagrams)
+   anagrams.select do |a| 
+   a.split("").sort == word.split("").sort
+  # binding.pry
+  end
+ end 
+ 
+end
+#match takes array of anagrams
+#match should return matches in an array
+#if no matches []
