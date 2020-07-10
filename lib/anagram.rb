e9ad30e75a1dff0a71d7d26794c4da3d8998fa1a
Charlie Barth <charliepbarth@gmail.com>
2020-07-10 18:57:04 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(array)
+    new_array = []
+    tests = @word.chars.sort.join
+    array.each do |name|
+      x = name.chars.sort.join
+      if tests == x
+        new_array << name
+      end
+    end
+    new_array
+  end
+end
