8295a36e47212488e311898377875136706cdce8
Hope Gipson <gipson.hope@gmail.com>
2020-07-13 22:55:49 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  
+  attr_accessor :word
+  
+  def initialize(word)
+    @word = word
+  end
+  
+  def match(wordarray)
+      wordarray.select do |wrd|
+      (@word.split("").sort) == (wrd.split("").sort)
+    end
+  end
+  
+  
+end
