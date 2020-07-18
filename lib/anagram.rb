a89d6beb331da98b8f34372fb32bc0167661c82f
DAMETRES PERRY <dametres.perry21@gmail.com>
2020-07-18 04:21:52 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,18 @@
 # Your code goes here!
+class Anagram
+ attr_accessor :word
+
+ def initialize(word)
+   @word = word
+ end
+
+ def match(anagrams)
+   anagrams.select do |a| 
+   a.split("").sort == word.split("").sort
+  end
+ end 
+ 
+end
+#match takes array of anagrams
+#match should return matches in an array
+#if no matches []
